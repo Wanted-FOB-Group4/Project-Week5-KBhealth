2e91dd898c44f038495017ae53b699201aa32cb6
chichoon <chichoon.choi@gmail.com>
2022-05-28 00:58:59 +0900

    Feat: hr 추가 (경계선)

--- src/routes/Dashboard/index.tsx
+++ src/routes/Dashboard/index.tsx
@@ -8,6 +8,8 @@ export const Dashboard = () => {
       <div className={styles.dashboardWrapper}>
         <DashBoardHeader />
         <main className={styles.dashboardMain}>
+          {/* TODO: 다른 컴포넌트 여기에 배치 */}
+          <hr />
           <MyHealthCare />
         </main>
       </div>
