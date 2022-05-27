2337be3d49d3de7cf4105ff53089a142da6e5e8d
chichoon <chichoon.choi@gmail.com>
2022-05-28 00:25:12 +0900

    Feat: SVG 아이콘 추가

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -1,1 +1,10 @@
-export {}
+export { ReactComponent as WifiIcon } from './wifiIcon.svg'
+export { ReactComponent as BloodPressureIcon } from './ic-icon-mission-h-1.svg'
+export { ReactComponent as BMIIcon } from './ic-icon-mission-h-2.svg'
+export { ReactComponent as AlcoholIcon } from './ic-icon-mission-h-3.svg'
+export { ReactComponent as GMRIcon } from './ic-icon-mission-h-4.svg'
+export { ReactComponent as CholesterolIcon } from './ic-icon-mission-h-5.svg'
+export { ReactComponent as SmokeIcon } from './ic-icon-mission-h-6.svg'
+export { ReactComponent as BloodSugarIcon } from './ic-icon-mission-h-7.svg'
+export { ReactComponent as ExerciseIcon } from './ic-icon-mission-h-8.svg'
+export { ReactComponent as BatteryIcon } from './batteryIcon.svg'
