0d76b774c299e96fcad509bff4b5e5cb5f30cee7
chichoon <chichoon.choi@gmail.com>
2022-05-30 11:24:38 +0900

    Feat: Chart 넣음

--- src/routes/Dashboard/index.tsx
+++ src/routes/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import MyHealthCare from './MyHealthCare'
+import ScoreListChart from './ScoreListChart'
 import UserHealthInfo from './UserHealthInfo'
 
 export const Dashboard = () => {
   return (
     <>
       <UserHealthInfo />
+      <ScoreListChart />
       <hr />
       <MyHealthCare />
     </>
