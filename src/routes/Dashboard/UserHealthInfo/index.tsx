2d70350888d2b01fc85c9147b2260f5cafe46109
chichoon <chichoon.choi@gmail.com>
2022-05-30 10:33:26 +0900

    Feat: UserHealthInfo routes

---
+++ src/routes/Dashboard/UserHealthInfo/index.tsx
@@ -0,0 +1,50 @@
+import { Link } from 'react-router-dom'
+
+import healthData from 'data/health.json'
+import { NotificationIcon } from 'assets/svgs'
+
+import styles from './userHealthInfo.module.scss'
+
+const UserHealthInfo = () => {
+  const {
+    userInfo: { healthDate },
+    wxcResultMap: {
+      paramMap: { sex, age, resHeight },
+    },
+  } = healthData
+
+  const checkDateList = healthDate.split('')
+  const checkDate = `${checkDateList.slice(0, 4).join('')}.${checkDateList.slice(4, 6).join('')}.${checkDateList
+    .slice(6, 8)
+    .join('')}`
+
+  return (
+    <section className={styles.container}>
+      <div className={styles.title}>마이 헬스</div>
+      <div className={styles.healthScore}>
+        <div className={styles.scoreTitle}>김헬스님의 건강점수</div>
+        <NotificationIcon className={styles.icon} />
+      </div>
+      <div className={styles.checkDate}>{checkDate}</div>
+      <button className={styles.updateResultBtn} type='button'>
+        건강검진결과 가져오기 &gt;
+      </button>
+      <div className={styles.userInfoBox}>
+        <div className={styles.userInfoTitle}>기본정보</div>
+        <div className={styles.userInfos}>
+          <div className={styles.userInfo}>{sex === '1' ? '남' : '여'}</div>
+          <div className={styles.userInfo}>{age}세</div>
+          <div className={styles.userInfo}>{resHeight}</div>
+        </div>
+      </div>
+      <div className={styles.analysisResult}>건강점수 분석 결과</div>
+      <button className={styles.detailResultBtn} type='button'>
+        <Link to='/details'>
+          <u>결과 자세히 보기</u> &gt;
+        </Link>
+      </button>
+    </section>
+  )
+}
+
+export default UserHealthInfo
