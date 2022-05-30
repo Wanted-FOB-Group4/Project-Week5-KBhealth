36a005a10d65348e665ce15836bf00a7299bb41d
HyeongSeoku <gudtjr3437@naver.com>
2022-05-30 13:26:39 +0900

    Feat : Add scatter

--- src/routes/Dashboard/DoubleChart/index.tsx
+++ src/routes/Dashboard/DoubleChart/index.tsx
@@ -1,51 +1,56 @@
-import { VictoryAxis, VictoryBar, VictoryChart, VictoryContainer, VictoryLine } from 'victory'
+import { VictoryAxis, VictoryBar, VictoryChart, VictoryContainer, VictoryLine, VictoryScatter } from 'victory'
 import { CallbackArgs } from 'victory-core'
 
 import styles from './doubleChart.module.scss'
 
 interface chartData {
-  dataList: number[]
+  category: string
+  value: number
 }
 
-const DUMMY_DATA_LIST = [
-  { category: '나', value: 750, fill: 'red' },
-  { category: '30대 남성', value: 800, fill: 'yellow' },
-]
-
 const CHART_COLOR = {
-  myColor: '#FFC001',
-  otherColor: '#BFBFBF',
+  bar: { myColor: '#FFC001', otherColor: '#FF833D' },
+  scatter: { myColor: '#BEBEBE', otherColor: '#ffffff' },
 }
 
 const STROK_COLOR = '#BEBEBE'
 
-const DoubleChart = () => {
-  // { data }: { data: chartData }
-  // const { dataList } = data
+const DoubleChart = ({ data }: { data: chartData[] }) => {
+  const setBarChartColor = (target: string) => {
+    const targetColor = target === '나' ? CHART_COLOR.bar.myColor : CHART_COLOR.bar.otherColor
+    return targetColor
+  }
 
-  const setChartColor = (target: string) => {
-    const targetColor = target === '나' ? CHART_COLOR.myColor : CHART_COLOR.otherColor
+  const setScatterChartColor = (target: string) => {
+    const targetColor = target === '나' ? CHART_COLOR.scatter.myColor : CHART_COLOR.scatter.otherColor
     return targetColor
   }
 
   return (
     <div className={styles.chartContainer}>
-      <VictoryChart containerComponent={<VictoryContainer />}>
+      <VictoryChart>
         <VictoryBar
-          data={DUMMY_DATA_LIST}
+          data={data}
           x='category'
           y='value'
           labels={({ datum }) => `${datum.value}`}
           style={{
-            data: { fill: ({ datum }: CallbackArgs) => setChartColor(datum.category) },
+            data: { fill: ({ datum }: CallbackArgs) => setBarChartColor(datum.category) },
           }}
         />
-        <VictoryLine data={DUMMY_DATA_LIST} x='category' y='value' style={{ data: { stroke: STROK_COLOR } }} />
+        <VictoryLine data={data} x='category' y='value' style={{ data: { stroke: STROK_COLOR } }} />
+        <VictoryScatter
+          data={data}
+          style={{
+            data: { fill: ({ datum }: CallbackArgs) => setScatterChartColor(datum.category) },
+            border: { stroke: 'black' },
+          }}
+          x='category'
+          y='value'
+        />
         <VictoryAxis
           style={{
-            axisLabel: { stroke: '#000000' },
             axis: { stroke: 'transparent' },
-            ticks: { stroke: 'transparent' },
           }}
         />
       </VictoryChart>
@@ -54,10 +59,3 @@ const DoubleChart = () => {
 }
 
 export default DoubleChart
-
-// style={{
-//   data: {
-//     fill: '#0074B7',
-//     strokeWidth: 1,
-//   },
-// }}
